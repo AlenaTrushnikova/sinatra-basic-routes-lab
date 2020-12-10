aabb71b37e43321eb9a8b4abf60e09ef8b164d22
Alena Trushnikova <alena.yu.t@gmail.com>
2020-12-10 11:32:52 -0800

    done

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-end
+  
+    get '/name' do
+      "My name is Alena!"
+    end
+  
+    get '/hometown' do
+      "My hometown is Perm, Russia!"
+    end
+  
+    get '/favorite-song' do
+      "My favorite song is Nightcall by Kavinsky!"
+    end
+  end
+  
